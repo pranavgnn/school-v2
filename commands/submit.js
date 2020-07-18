ebc3c1a3726169652b52f420d6c2f4b37666b729
pranvexploder <56198908+pranvexploder@users.noreply.github.com>
2020-07-18 20:58:01 +0530

    Add files via upload

--- commands/submit.js
+++ commands/submit.js
@@ -1,3 +1,8 @@
+const Discord = require('discord.js')
+const request = require(`request`);
+const fs = require(`fs`);
+const { measureMemory } = require('vm');
+
 exports.config = {
     name: "submit",
     description: "A command that will help you with submitting assignments.",
@@ -6,17 +11,15 @@ exports.config = {
 };
 
 exports.run = async (bot, message, args) => {
+    var currentAttachs = []
     var assignmentName = args.join(' ')
     if (!assignmentName) return message.channel.send('Please specify the assignment name!')
     var attachs = message.attachments
     if (!attachs.array()[0]) return message.channel.send('Please attach the notes in this message as assignments!')
-    var msg = `<@${message.author.id}> (${message.author.tag}) has submitted the answer(s) for **${assignmentName}**!\n`
-    for (let i = 0; i < attachs.array().length; i++) {
-        msg = msg + attachs.array()[i].url + "\n"
-    };
+    for (let i = 0; i < attachs.array().length; i++) currentAttachs.push(attachs.array()[i].url)
 
     for (let user of message.guild.members.cache.array()) {
-        try { user.send(msg) }
-        catch { err => { if (err) throw err } }
+        user.send(`**${args.join(" ")}** | By <@${message.author.id}> (${message.author.tag})`,{files: currentAttachs}).catch(console.log)
     }
+    
 };
--- modules/getUserFromMention.js
+++ modules/getUserFromMention.js
@@ -1,7 +1,7 @@
-exports = async (argument, guild) => {
+module.exports = (argument, guild) => {
     var id = argument.slice(1, argument.length - 1)
     if (id.startsWith('!')) id = id.slice(1)
     var user = guild.members.cache.find(m => m.id === id)
     if (!user) return 'The member you mentioned is either invalid or isn\'t in this server!'
     else return user
-}
+}   
